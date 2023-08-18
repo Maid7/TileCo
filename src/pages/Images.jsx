5d9d5aca96f638b90a6c7dc89b4bffc5b81aa1a9
Maid7 <93914189+Maid7@users.noreply.github.com>
2023-08-18 14:00:07 +0200

    again changed some images

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -6,17 +6,17 @@ const Images = () => {
   const [fullscreenImage, setFullscreenImage] = useState(null);
 const images = [
   "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-  "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-   "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-   "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-   "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-    "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-     "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-     "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-     "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-     "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-     "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
-      "https://cdn.mos.cms.futurecdn.net/BPdZxKcA8Dc8xW4uxjWLGY.jpg",
+  "https://images.pexels.com/photos/1457847/pexels-photo-1457847.jpeg?cs=srgb&dl=pexels-jean-van-der-meulen-1457847.jpg&fm=jpg",
+   "https://www.designlike.com/wp-content/uploads/2021/02/pexels-photo-6207818.jpeg",
+   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbnVGdeCDfJUk4P-m6Z8bKQkgCIGVn0NApLqwbVuPC3VNuFrve6YIll_RtY1zXPSapQq0&usqp=CAU",
+   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqm7oqm6mWhyvq9Ts5AbgxnbT-F9n5ukLdAQ&usqp=CAU",
+    "https://cottagelife.com/wp-content/uploads/2021/01/shutterstock_50943826.jpg",
+     "https://media.angi.com/s3fs-public/installing-ceramic-tile-floor.jpg",
+     "https://www.britishceramictile.com/image?w=970&h=545&fit=crop-50-50&src=production%2Fcms%2Fprocessed%2F3b3b4e43ff07974f9ced5d3e5043ee54.jpg&s=9a692a2e157f74b06360c149b23159e3",
+     "https://www.betterbathrooms.com/Images/BeBa_15641_1_supersize.jpg?v=2",
+     "https://5.imimg.com/data5/SELLER/Default/2021/8/BL/NL/VR/135337296/bathroom-wall-tiles.jpg",
+     "https://cottagelife.com/wp-content/uploads/2021/01/shutterstock_50943826.jpg",
+      "https://static5.depositphotos.com/1026645/398/i/450/depositphotos_3982522-Bathroom-with-shower-corner.jpg",
 ]
 const toggleFullscreen = (image) => {
   if (fullscreenImage === image) {
