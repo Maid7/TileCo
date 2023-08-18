f84091208b5fc5951248c7e1d26d16a4a0461364
Maid7 <93914189+Maid7@users.noreply.github.com>
2023-08-18 11:25:51 +0200

    changed images location from public to src

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -5,18 +5,18 @@ import { SimpleGrid,Box,Heading} from '@chakra-ui/react'
 const Images = () => {
   const [fullscreenImage, setFullscreenImage] = useState(null);
 const images = [
-  "/src/images/20210709_143538.jpg",
-  "/src/images/20210709_143557.jpg",
-  "/src/images/20210709_143605.jpg",
-  "/src/images/20210711_131052.jpg",
-  "/src/images/20210716_115620.jpg",
-  "/src/images/20220528_080358.jpg",
-  "/src/images/20220528_080401.jpg",
-  "/src/images/20220528_080443.jpg",
-  "/src/images/20220528_080516.jpg",
-  "/src/images/20220531_094834.jpg",
-  "/src/images/20220602_174722.jpg",
-  "/src/images/20220610_151748.jpg"
+  "/src/images/assets/20210709_143538.jpg",
+  "/src/images/assets/20210709_143557.jpg",
+  "/src/images/assets/20210709_143605.jpg",
+  "/src/images/assets/20210711_131052.jpg",
+  "/src/images/assets/20210716_115620.jpg",
+  "/src/images/assets/20220528_080358.jpg",
+  "/src/images/assets/20220528_080401.jpg",
+  "/src/images/assets/20220528_080443.jpg",
+  "/src/images/assets/20220528_080516.jpg",
+  "/src/images/assets/20220531_094834.jpg",
+  "/src/images/assets/20220602_174722.jpg",
+  "/src/images/assets/20220610_151748.jpg"
 ]
 const toggleFullscreen = (image) => {
   if (fullscreenImage === image) {
